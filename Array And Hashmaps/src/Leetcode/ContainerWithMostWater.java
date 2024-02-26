df321fc99549b5e2f7f8fe1ccfa0eabfd67ba69f
Tarang Patel <tarang5757@gmail.com>
2024-02-26 13:56:41 -0500

    Added ContainerWithMostWater

---
+++ Array And Hashmaps/src/Leetcode/ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+package Leetcode;
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int pointerA = 0;
+        int pointerB = height.length - 1;
+        int max_area = 0;
+
+        for (int i = 0; i < height.length; i++) {
+            int width = pointerB - pointerA;
+            int waterHeight = 0;
+
+            if (height[pointerA] < height[pointerB]) {
+                waterHeight = height[pointerA];
+                max_area = Math.max(max_area, waterHeight * width);
+                pointerA++;
+            }
+
+            else {
+                waterHeight = height[pointerB];
+                max_area = Math.max(max_area, waterHeight * width);
+                pointerB--;
+            }
+
+        }
+        return max_area;
+
+    }
+
+}
